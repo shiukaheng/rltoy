32fd706ba93ef6de8b28b11cc57058d0ed28341c
Heng <31088159+shiukaheng@users.noreply.github.com>
2026-08-18 15:51:51 +0100

    cart pole policy draft

--- src/rltoy/exercises/cartpole.py
+++ src/rltoy/exercises/cartpole.py
@@ -1,7 +1,26 @@
 import gymnasium as gym
+import torch.nn as nn
 
 env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset()
+
+class CartPolePolicy(nn.Module):
+    def __init__(self):
+        self.layers = nn.Sequential(
+            nn.Linear(4,4),
+            nn.ReLU(),
+            nn.Linear(4,4),
+            nn.ReLU(),
+            nn.Linear(4,2),
+            nn.Softmax()
+        )
+    def forward(self, state):
+        """
+        (4,): (pos, vel, ang, ang_vel) ->
+        (2,): (left_logit, right_logit)
+        """
+        return self.layers(state)
+
 
 for _ in range(1_000):
     action = env.action_space.sample()  # Random policy.
